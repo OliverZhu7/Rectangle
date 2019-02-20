b91586e8b248b1d3be3e9b287468367da1f969e5
jingyq1 <31482058+jingyq1@users.noreply.github.com>
2019-02-19 23:13:21 -0500

    Update databaseTest.java

--- Rectangle1/src/Rectangle1/databaseTest.java
+++ Rectangle1/src/Rectangle1/databaseTest.java
@@ -22,53 +22,54 @@ public class databaseTest extends student.TestCase {
     }
 
 
-// /**
-// * this is the method test the getName method
-// */
-// public void testInsert() {
-// assertTrue(base.insert("ok1", 1, 1, 1, 1));
-// assertTrue(base.insert("ok1", 1, 1, 1, 1));
-// assertFalse(base.insert("xles", -1, 1, 1, 1));
-// assertFalse(base.insert("yles", 1, -1, 1, 1));
-// assertFalse(base.insert("wles", 1, 1, -1, 1));
-// assertFalse(base.insert("hles", 1, 1, 1, -1));
-// assertFalse(base.insert("xlar", 1, 1, 2000, 1));
-// assertFalse(base.insert("ylar", 1, 1, 1, 2000));
-// }
-//
-//
-// /**
-// * THIS IS the method to test the insertHelp method
-// */
-//
-// public void testInsertHelp() {
-// assertTrue(base.insert("eee", 1, 1, 1, 1));
-// assertTrue(base.insert("ddd", 1, 1, 1, 1));
-// assertTrue(base.insert("ddd", 1, 1, 1, 1));
-// assertTrue(base.insert("fff", 1, 1, 1, 1));
-// assertTrue(base.insert("fff", 1, 1, 1, 1));
-// }
-//
-//
-// /**
-// * this method is used to test the search method
-// */
-// public void testSearch() {
-// assertFalse(base.search("233"));
-// base.insert("eee", 1, 1, 1, 1);
-// assertFalse(base.search("233"));
-// assertTrue(base.search("eee"));
-// }
-//
-//
-// public void testSearchHelp() {
-// base.insert("eee", 1, 1, 1, 1);
-// base.insert("ddd", 1, 1, 1, 1);
-// base.insert("fff", 1, 1, 1, 1);
-// assertTrue(base.search("eee"));
-// assertTrue(base.search("ddd"));
-// assertTrue(base.search("fff"));
-// }
+    /**
+     * this is the method test the getName method
+     */
+    public void testInsert() {
+        assertTrue(base.insert("ok1", 1, 1, 1, 1));
+        assertTrue(base.insert("ok1", 1, 1, 1, 1));
+        assertFalse(base.insert("xles", -1, 1, 1, 1));
+        assertFalse(base.insert("yles", 1, -1, 1, 1));
+        assertFalse(base.insert("wles", 1, 1, -1, 1));
+        assertFalse(base.insert("hles", 1, 1, 1, -1));
+        assertFalse(base.insert("xlar", 1, 1, 2000, 1));
+        assertFalse(base.insert("ylar", 1, 1, 1, 2000));
+    }
+
+
+    /**
+     * THIS IS the method to test the insertHelp method
+     */
+
+    public void testInsertHelp() {
+        assertTrue(base.insert("eee", 1, 1, 1, 1));
+        assertTrue(base.insert("ddd", 1, 1, 1, 1));
+        assertTrue(base.insert("ddd", 1, 1, 1, 1));
+        assertTrue(base.insert("fff", 1, 1, 1, 1));
+        assertTrue(base.insert("fff", 1, 1, 1, 1));
+    }
+
+
+    /**
+     * this method is used to test the search method
+     */
+    public void testSearch() {
+        assertFalse(base.search("233"));
+        base.insert("eee", 1, 1, 1, 1);
+        assertFalse(base.search("233"));
+        assertTrue(base.search("eee"));
+    }
+
+
+    public void testSearchHelp() {
+        base.insert("eee", 1, 1, 1, 1);
+        base.insert("ddd", 1, 1, 1, 1);
+        base.insert("fff", 1, 1, 1, 1);
+        assertTrue(base.search("eee"));
+        assertTrue(base.search("ddd"));
+        assertTrue(base.search("fff"));
+    }
+
 
     /**
      * this method is used to test the remove method
@@ -92,6 +93,9 @@ public class databaseTest extends student.TestCase {
     }
 
 
+    /**
+     * test
+     */
     public void testRemoveHelp2() {
 
         base.insert("eee", 1, 1, 1, 1);
@@ -108,6 +112,9 @@ public class databaseTest extends student.TestCase {
     }
 
 
+    /**
+     * test
+     */
     public void testRemoveHelp() {
         base.insert("eee", 1, 1, 1, 1);
         assertFalse(base.remove("zzz"));
@@ -143,7 +150,7 @@ public class databaseTest extends student.TestCase {
     /**
      * this method is used to test the intersection method
      */
-    public void testIntersection() {
+    public void testRegionSearchHelp() {
         base.insert("eee", 1, 1, 1, 1);
         base.insert("ddd", 1, 1, 1, 1);
         base.insert("fff", 1, 1, 1, 1);
@@ -157,10 +164,54 @@ public class databaseTest extends student.TestCase {
 
 
     /**
+     * test
+     */
+    public void testIntersections() {
+        assertFalse(base.intersections());
+        base.insert("eee", 1, 1, 1, 1);
+        base.insert("ddd", 2, 2, 1, 1);
+        base.insert("fff", 3, 3, 1, 1);
+        base.insert("ddd", 4, 4, 1, 1);
+        base.insert("fff", 5, 5, 1, 1);
+        base.insert("ccc", 6, 6, 1, 1);
+        base.insert("eee", 7, 7, 1, 1);
+        assertFalse(base.intersections());
+    }
+
+
+    /**
+     * test
+     */
+    public void testIntersectionsHelp() {
+        base.insert("eee", 1, 1, 1, 1);
+        base.insert("ddd", 2, 2, 1, 1);
+        base.insert("fff", 3, 3, 1, 1);
+        base.insert("ddd", 4, 4, 1, 1);
+        base.insert("fff", 5, 5, 1, 1);
+        base.insert("ccc", 6, 6, 1, 1);
+        base.insert("eee", 7, 7, 1, 1);
+        assertFalse(base.intersections());
+    }
+
+
+    /**
      * this method is used to test the dump method
      */
     public void testDump() {
-
+        assertFalse(base.dump());
+        base.insert("eee", 1, 1, 1, 1);
+        assertTrue(base.dump());
     }
 
+
+    public void testDumpHelp() {
+        base.insert("eee", 1, 1, 1, 1);
+        base.insert("ccc", 2, 2, 1, 1);
+        base.insert("fff", 3, 3, 1, 1);
+        base.insert("ddd", 4, 4, 1, 1);
+        base.insert("fff", 5, 5, 1, 1);
+        base.insert("aaa", 6, 6, 1, 1);
+        base.insert("eee", 7, 7, 1, 1);
+        assertTrue(base.dump());
+    }
 }
