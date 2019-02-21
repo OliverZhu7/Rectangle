81d52e76a9a27bfc723b334f0f011c38e7c1958a
Oliver Zhu <30608978+OliverZhu7@users.noreply.github.com>
2019-02-21 18:48:14 -0500

    Update nodeTest.java

--- Rectangle1/src/Rectangle1/nodeTest.java
+++ Rectangle1/src/Rectangle1/nodeTest.java
@@ -1,52 +1,56 @@
-package Rectangle1;
 
 import junit.framework.TestCase;
 
-public class nodeTest extends TestCase {
-	private node node1;
-	private node node2;
-	private node node3;
-	private node node4;
-	private node node5;
-	private node node6;
-	
-	/**
-	 * this is the pre set up
-	 */
-	public void setUp() {
-	 node1 = new node("first",node2,node3);
-	 node2 = new node("second",null,null);
-	 node3 = new node("third",null,null);
-	 node4 = new node("fourth",null,null);
-	 node5 = new node("fifth",null,null);
-	 node6 = new node(null,null,null);
-	 
-	}
-	
-	/**
-	 * this is the method test the getName method
-	 */
-	public void testsetRight() {
-		node3.setRight(node4);
-		assertEquals(node3.getRight(),node4);		
-	}
-	
-	/**
-	 * this is the method test the getName method
-	 */
-	public void testsetLeft() {
-		node3.setLeft(node4);
-		assertEquals(node3.getLeft(),node4);	
-	}
-	
-	/**
-	 * this is the method test the getName method
-	 */
-	public void testsetElement() {
-		node6.setElement("sixth");
-		assertEquals(node6.getElement(),"sixth");
-	}
-	
-	
+/**
+ * this the test class for rectangles class
+ * 
+ * @author Zichen Zhu(zichen), Jingyuan Qi(jingyq1)
+ * @version this is the version 2/19/2019
+ *
+ */
+public class NodeTest extends TestCase {
+    // private Node<Rectangles> node1;
+    // private Node<Rectangles> node2;
+    private Node<Rectangles> node3;
+    private Node<Rectangles> node4;
+    // private Node<Rectangles> node6;
+    private Rectangles rect1;
+
+    /**
+     * this is the pre set up
+     */
+    public void setUp() {
+        // node1 = new Node("first", node2, node3);
+
+        // node2 = new Node("second", null, null);
+        node3 = new Node("third", null, null);
+        node4 = new Node("fourth", null, null);
+        // node6 = new Node<Rectangles>(null, null, null);
+    }
+
+    /**
+     * this is the method test the getName method
+     */
+    public void testsetRight() {
+        node3.setRight(node4);
+        assertEquals(node3.getRight(), node4);
+    }
+
+    /**
+     * this is the method test the getName method
+     */
+    public void testsetLeft() {
+        node3.setLeft(node4);
+        assertEquals(node3.getLeft(), node4);
+    }
+
+    /**
+     * this is the method test the getName method
+     */
+    public void testsetElement() {
+        Node<Rectangles> node6 = new Node<Rectangles>(null, null, null);
+        node6.setElement(rect1);
+        assertEquals(node6.getElement(), rect1);
+    }
 
 }
