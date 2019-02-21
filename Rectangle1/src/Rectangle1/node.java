f24acc29135067685b45024dc9b7d3335adb83da
Oliver Zhu <30608978+OliverZhu7@users.noreply.github.com>
2019-02-21 18:47:33 -0500

    Update node.java

--- Rectangle1/src/Rectangle1/node.java
+++ Rectangle1/src/Rectangle1/node.java
@@ -1,81 +1,86 @@
-package Rectangle1;
 
-public class node<T> {
+/**
+ * 
+ * @author Zichen Zhu(zichen), Jingyuan Qi(jingyq1)
+ * @version this is the version
+ * @param <T>
+ */
+public class Node<T> {
     private T element;
-    private node Right;
-    private node Left;
+    private Node<T> right;
+    private Node<T> left;
+    // private Node<T><Rectangles> abc;
     // private Rectangles Parents;
 
     /**
-     * 
+     * this is the Node
      */
-    public node() {
+    public Node() {
         element = null;
-        Right = null;
-        Left = null;
+        right = null;
+        left = null;
     }
 
     /**
-     * 
+     * @param object    is the node
+     * @param rightSide is the rightNode
+     * @param leftSide  is the left node
      */
-    public node(T object, node rightSide, node leftSide) {
+    public Node(T object, Node<T> rightSide, Node<T> leftSide) {
         element = object;
-        Right = rightSide;
-        Left = leftSide;
+        right = rightSide;
+        left = leftSide;
     }
 
     /**
-     * set the right side child of this node
+     * set the right side child of this Node<T>
      * 
-     * @param RightIn
-     *            right side of this node
+     * @param rightIn right side of this Node<T>
      */
-    public void setRight(node RightIn) {
-        Right = RightIn;
+    public void setRight(Node<T> rightIn) {
+        right = rightIn;
     }
 
     /**
-     * set the left side child of this node
+     * set the left side child of this Node<T>
      * 
-     * @param LeftIn
-     *            left side of this node
+     * @param leftIn left side of this Node<T>
      */
-    public void setLeft(node LeftIn) {
-        Left = LeftIn;
+    public void setLeft(Node<T> leftIn) {
+        left = leftIn;
     }
 
     /**
      * set the element
      * 
-     * @param elementIn
-     *            rectangle in this node
+     * @param elementIn rectangle in this Node<T>
      */
     public void setElement(T elementIn) {
         element = elementIn;
     }
 
     /**
-     * get the right side child of this node.
+     * get the right side child of this Node<T>.
      * 
      * @return right side child
      */
-    public node getRight() {
-        return Right;
+    public Node<T> getRight() {
+        return right;
     }
 
     /**
-     * get the left side child of this node
+     * get the left side child of this Node<T>
      * 
      * @return left side child
      */
-    public node getLeft() {
-        return Left;
+    public Node<T> getLeft() {
+        return left;
     }
 
     /**
-     * get the rectangle of this node
+     * get the rectangle of this Node<T>
      * 
-     * @return element of this node
+     * @return element of this Node<T>
      */
     public T getElement() {
         return element;
@@ -86,9 +91,9 @@ public class node<T> {
     // *
     // * @param another
     // * rectangle's name
-    // * @return if this rectangle is larger or equal to another, return True
+    // * @return if this rectangle is larger or equal to another,return True
     // */
-    // public boolean compareTo(node another) {
+    // public boolean compareTo(Node<T> another) {
     // return ((Rectangles)this.getElement()).getName().compareTo(
     // ((Rectangles)another.getElement()).getName()) >= 0;
     // }
