e8cff24630d0b4ce3cfab3da2b0c8030ba05bf33
Oliver Zhu <30608978+OliverZhu7@users.noreply.github.com>
2019-02-21 18:47:04 -0500

    Update databaseTest.java

--- Rectangle1/src/Rectangle1/databaseTest.java
+++ Rectangle1/src/Rectangle1/databaseTest.java
@@ -1,15 +1,17 @@
-import student.TestCase;
+
+//import student.TestCase;
+
+import junit.framework.TestCase;
+
 
 /**
  * @version this is the version for 2/19/2019
- * @author zichen
- *         this class is used to test the database class
+ * @author Zichen Zhu(zichen), Jingyuan Qi(jingyq1)
  */
 
-public class databaseTest extends student.TestCase {
+public class DatabaseTest extends TestCase {
 
-    private database base;
-
+    private Database base;
 
     /**
      * this is the pre set up
@@ -18,14 +20,23 @@ public class databaseTest extends student.TestCase {
 
     public void setUp() {
 
-        base = new database();
+        base = new Database();
     }
-
 
     /**
      * this is the method test the getName method
      */
+    
     public void testInsert() {
+        assertTrue(base.insert("topleftCorner", 0, 0, 7, 6));
+    }
+    
+    /**
+     * this is the method to test insert
+     */
+
+    public void testInsert1() {
+        System.out.println("11111111111111111111111111111");
         assertTrue(base.insert("ok1", 1, 1, 1, 1));
         assertTrue(base.insert("ok1", 1, 1, 1, 1));
         assertFalse(base.insert("xles", -1, 1, 1, 1));
@@ -36,12 +47,12 @@ public class databaseTest extends student.TestCase {
         assertFalse(base.insert("ylar", 1, 1, 1, 2000));
     }
 
-
     /**
      * THIS IS the method to test the insertHelp method
      */
 
     public void testInsertHelp() {
+        System.out.println("222222222222222222222222");
         assertTrue(base.insert("eee", 1, 1, 1, 1));
         assertTrue(base.insert("ddd", 1, 1, 1, 1));
         assertTrue(base.insert("ddd", 1, 1, 1, 1));
@@ -49,19 +60,22 @@ public class databaseTest extends student.TestCase {
         assertTrue(base.insert("fff", 1, 1, 1, 1));
     }
 
-
     /**
      * this method is used to test the search method
      */
     public void testSearch() {
+        System.out.println("33333333333333333333333333333");
         assertFalse(base.search("233"));
         base.insert("eee", 1, 1, 1, 1);
         assertFalse(base.search("233"));
         assertTrue(base.search("eee"));
     }
 
-
+    /**
+     * this is the method to test
+     */
     public void testSearchHelp() {
+        System.out.println("4444444444444444444444444444");
         base.insert("eee", 1, 1, 1, 1);
         base.insert("ddd", 1, 1, 1, 1);
         base.insert("fff", 1, 1, 1, 1);
@@ -70,11 +84,11 @@ public class databaseTest extends student.TestCase {
         assertTrue(base.search("fff"));
     }
 
-
     /**
      * this method is used to test the remove method
      */
     public void testRemove() {
+        System.out.println("5555555555555555555555555555555");
         assertFalse(base.remove("233"));
         base.insert("eee", 1, 1, 1, 1);
         assertTrue(base.remove("eee"));
@@ -92,12 +106,11 @@ public class databaseTest extends student.TestCase {
         // only ddd
     }
 
-
     /**
      * test
      */
     public void testRemoveHelp2() {
-
+        System.out.println("6666666666666666666666666666");
         base.insert("eee", 1, 1, 1, 1);
         assertFalse(base.remove(2, 3, 3, 3));
         base.insert("ddd", 2, 1, 1, 1);
@@ -111,11 +124,11 @@ public class databaseTest extends student.TestCase {
         assertFalse(base.remove(3, 3, 33, 3));
     }
 
-
     /**
      * test
      */
     public void testRemoveHelp() {
+        System.out.println("7777777777777777777777777");
         base.insert("eee", 1, 1, 1, 1);
         assertFalse(base.remove("zzz"));
         assertFalse(base.remove("aaa"));
@@ -133,11 +146,11 @@ public class databaseTest extends student.TestCase {
         assertTrue(base.remove("ccc"));
     }
 
-
     /**
      * this method is used to test the region search
      */
     public void testRegionSearch() {
+        System.out.println("8888888888888888888888888");
         assertFalse(base.regionsearch(1, 1, -1, 1));
         assertFalse(base.regionsearch(1, 1, -1, -1));
         assertFalse(base.regionsearch(1, 1, 1, -1));
@@ -146,11 +159,11 @@ public class databaseTest extends student.TestCase {
         assertTrue(base.regionsearch(1, 1, 1, 5));
     }
 
-
     /**
      * this method is used to test the intersection method
      */
     public void testRegionSearchHelp() {
+        System.out.println("9999999999999999999999999999999999");
         base.insert("eee", 1, 1, 1, 1);
         base.insert("ddd", 1, 1, 1, 1);
         base.insert("fff", 1, 1, 1, 1);
@@ -162,11 +175,11 @@ public class databaseTest extends student.TestCase {
         assertTrue(base.regionsearch(1, 1, 1, 1));
     }
 
-
     /**
      * test
      */
     public void testIntersections() {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         assertFalse(base.intersections());
         base.insert("eee", 1, 1, 1, 1);
         base.insert("ddd", 2, 2, 1, 1);
@@ -177,12 +190,12 @@ public class databaseTest extends student.TestCase {
         base.insert("eee", 7, 7, 1, 1);
         assertFalse(base.intersections());
     }
-
 
     /**
      * test
      */
     public void testIntersectionsHelp() {
+        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
         base.insert("eee", 1, 1, 1, 1);
         base.insert("ddd", 2, 2, 1, 1);
         base.insert("fff", 3, 3, 1, 1);
@@ -193,18 +206,21 @@ public class databaseTest extends student.TestCase {
         assertFalse(base.intersections());
     }
 
-
     /**
      * this method is used to test the dump method
      */
     public void testDump() {
+        System.out.println("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
         assertFalse(base.dump());
         base.insert("eee", 1, 1, 1, 1);
         assertTrue(base.dump());
     }
 
-
+    /**
+     * this is the method used to test the dump
+     */
     public void testDumpHelp() {
+        System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
         base.insert("eee", 1, 1, 1, 1);
         base.insert("ccc", 2, 2, 1, 1);
         base.insert("fff", 3, 3, 1, 1);
@@ -213,5 +229,20 @@ public class databaseTest extends student.TestCase {
         base.insert("aaa", 6, 6, 1, 1);
         base.insert("eee", 7, 7, 1, 1);
         assertTrue(base.dump());
+    }
+    
+    /**
+     * this is the method to test the region search, without the valid region
+     */
+    public void testregionSearch1() {
+        System.out.println("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+        base.insert("eee", 1, 1, 1, 1);
+        base.insert("ddd", 1, 1, 1, 1);
+        base.insert("fff", 1, 1, 1, 1);
+        base.insert("ddd", 1, 1, 1, 1);
+        base.insert("ccc", 1, 1, 1, 1);
+        base.insert("fff", 1, 1, 1, 1);
+        base.insert("eee", 1, 1, 1, 1);
+        assertFalse(base.regionsearch(100, 100, 2, 1));
     }
 }
